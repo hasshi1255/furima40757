ef5937298234810edd56cbdf8c7de43f31e6f65b
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-08-08 15:30:38 +0900

    エラーメッセージ日本語を削除

--- app/models/user.rb
+++ app/models/user.rb
@@ -12,16 +12,16 @@ class User < ApplicationRecord
 
   # パスワードが必須であること、6文字以上であること、半角英数字混合であること
   validates :password, presence: true, length: { minimum: 6 },
-                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: '文字と数字の両方を含める必要があります' }
+                       format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 
   # パスワードとパスワード（確認）が一致していること
   validates :password_confirmation, presence: true, on: :create
 
   # お名前(全角)が必須であること、全角（漢字・ひらがな・カタカナ）での入力が必須であること
-  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字である必要があります' }
+  validates :last_name, :first_name, presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
 
   # お名前カナ(全角)が必須であること、全角（カタカナ）での入力が必須であること
-  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/, message: '全角カタカナである必要があります' }
+  validates :last_name_kana, :first_name_kana, presence: true, format: { with: /\A[ァ-ヶー－]+\z/ }
 
   # 生年月日が必須であること
   validates :birth_date, presence: true
