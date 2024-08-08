e8a5998b06e1638083cb127efdd277b5a5b1464e
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-08-08 15:21:01 +0900

    ログイン・ログアウト

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth_date])
+  end
 
   private
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, controllers: {
-  registrations: 'registrations'
-}
+  devise_for :users
+  resources :users
   root to: 'items#index'
 end
