0b842e09997561be47bee723267f083323a11c0d
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-08-17 20:21:41 +0900

    「---」に相当するidを異常値としてテストを実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -9,6 +9,11 @@ RSpec.describe Item, type: :model do
   describe '商品出品' do
     context '商品が出品できるとき' do
       it '全ての項目が正しく入力されていれば、出品できる' do
+        @item.category_id = 2 # 仮に2が正しいIDとします
+        @item.condition_id = 2
+        @item.shipping_cost_id = 2
+        @item.prefecture_id = 2
+        @item.shipping_day_id = 2
         expect(@item).to be_valid
       end
     end
@@ -29,37 +34,37 @@ RSpec.describe Item, type: :model do
       it '商品の説明が空では出品できない' do
         @item.description = ''
         @item.valid?
-        expect(@item.errors.full_messages).to include("Description can't be blank") # 修正
+        expect(@item.errors.full_messages).to include("Description can't be blank")
       end
 
-      it 'カテゴリーが空では出品できない' do
-        @item.category_id = nil
+      it 'カテゴリーが未選択では出品できない' do
+        @item.category_id = 1 # `---` に相当する id
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category can't be blank")
+        expect(@item.errors.full_messages).to include('Category を選択してください')
       end
 
-      it '商品の状態が空では出品できない' do
-        @item.condition_id = nil
+      it '商品の状態が未選択では出品できない' do
+        @item.condition_id = 1 # `---` に相当する id
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition can't be blank") # 修正
+        expect(@item.errors.full_messages).to include('Condition を選択してください')
       end
 
-      it '配送料の負担が空では出品できない' do
-        @item.shipping_cost_id = nil
+      it '配送料の負担が未選択では出品できない' do
+        @item.shipping_cost_id = 1 # `---` に相当する id
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping cost can't be blank") # 修正
+        expect(@item.errors.full_messages).to include('Shipping cost を選択してください')
       end
 
-      it '発送元の地域が空では出品できない' do
-        @item.prefecture_id = nil
+      it '発送元の地域が未選択では出品できない' do
+        @item.prefecture_id = 1 # `---` に相当する id
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture can't be blank")
+        expect(@item.errors.full_messages).to include('Prefecture を選択してください')
       end
 
-      it '発送までの日数が空では出品できない' do
-        @item.shipping_day_id = nil
+      it '発送までの日数が未選択では出品できない' do
+        @item.shipping_day_id = 1 # `---` に相当する id
         @item.valid?
-        expect(@item.errors.full_messages).to include("Shipping day can't be blank") # 修正
+        expect(@item.errors.full_messages).to include('Shipping day を選択してください')
       end
 
       it '価格が空では出品できない' do
@@ -81,7 +86,7 @@ RSpec.describe Item, type: :model do
       end
 
       it '価格が半角数値でない場合、出品できない' do
-        @item.price = '１０００'
+        @item.price = '１０００' # 全角数字
         @item.valid?
         expect(@item.errors.full_messages).to include('Price is not a number')
       end
