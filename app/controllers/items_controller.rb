0dae2e324221ab7e81a31ee60fa87cff1b3d5aa1
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-08-17 17:42:47 +0900

    Itemコントローラの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,32 @@
 class ItemsController < ApplicationController
-  def index
+  before_action :authenticate_user!, only: [:new, :create]
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path, notice: '商品が出品されました'
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image,
+      :name,
+      :description,
+      :category_id,
+      :condition_id,
+      :shipping_cost_id,
+      :prefecture_id,
+      :shipping_day_id,
+      :price
+    ).merge(user_id: current_user.id)
   end
 end
