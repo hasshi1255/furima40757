4cddeb34b9f1b87d666c172c085d3a6fdc30afec
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-06 14:48:00 +0900

    ビューの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -1,7 +1,39 @@
 class PurchasesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_item, only: [:index, :create]
+  before_action :redirect_if_item_sold_out_or_owned, only: [:index]
+
   def index
+    @item = Item.find(params[:item_id])
+    @purchase = Purchase.new
+
+    render 'orders/index'
   end
 
   def create
+    @purchase = Purchase.new(order_params)
+    @purchase.item = @item
+
+    if @purchase.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
+  def redirect_if_item_sold_out_or_owned
+    return unless @item.sold_out? || @item.user == current_user
+
+    redirect_to root_path
+  end
+
+  def order_params
+    params.require(:purchase).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'purchases/index'
-  get 'purchases/create'
   devise_for :users
   resources :users
 
   resources :items do
     resources :purchases, only: [:index, :create] 
   end
+
   
   root to: 'items#index'
   
