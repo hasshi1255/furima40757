f174e63d7c288b312773f90e71ff6bfc8e0ae5b8
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-06 12:47:23 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
   resources :users
-  resources :items
+
+  resources :items do
+    resources :purchases, only: [:index, :create] 
+  end
   
   root to: 'items#index'
   
