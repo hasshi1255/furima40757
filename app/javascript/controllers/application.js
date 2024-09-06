197ea4a93c2b7a5278a74df6762289e1e32d0ada
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-06 15:16:00 +0900

    payjp.jsの読み込み

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -3,6 +3,10 @@ import { Application } from "@hotwired/stimulus"
 const application = Application.start()
 
 // Configure Stimulus development experience
+import "@hotwired/turbo-rails"
+import "controllers"
+import "card"
+
 application.debug = false
 window.Stimulus   = application
 
