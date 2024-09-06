d65d86a31b02d8c89aec4e17c028bcb689ca9962
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-06 18:04:12 +0900

    コントローラーの編集

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -4,18 +4,25 @@ class PurchasesController < ApplicationController
   before_action :redirect_if_item_sold_out_or_owned, only: [:index]
 
   def index
-    @item = Item.find(params[:item_id])
     @purchase = Purchase.new
+    @shipping_address = ShippingAddress.new
+    @item = Item.find(params[:item_id])
 
     render 'orders/index'
   end
 
   def create
-    @purchase = Purchase.new(order_params)
-    @purchase.item = @item
+    @purchase = Purchase.new(user: current_user, item: @item)
 
-    if @purchase.save
-      redirect_to root_path
+    if @purchase.valid?
+      pay_item
+      @shipping_address = @purchase.build_shipping_address(order_params)
+
+      if @shipping_address.save
+        redirect_to root_path, notice: '購入が完了しました。'
+      else
+        render :index, status: :unprocessable_entity
+      end
     else
       render :index, status: :unprocessable_entity
     end
@@ -34,6 +41,16 @@ class PurchasesController < ApplicationController
   end
 
   def order_params
-    params.require(:purchase).permit(:postal_code, :prefecture_id, :city, :address, :building_name, :phone_number)
+    params.require(:purchase).permit(:postal_code, :prefecture_id, :city, :address, :building_name,
+                                     :phone_number).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = 'sk_test_5189c670933163d318dc124d' # 自身のPAY.JPテスト秘密鍵を記述しましょう
+    Payjp::Charge.create(
+      amount: @item.price, # 商品の値段
+      card: order_params[:token], # カードトークン
+      currency: 'jpy' # 通貨の種類（日本円）
+    )
   end
 end
