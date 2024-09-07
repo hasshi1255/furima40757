0ab86095a5e4a61c20f830eb6cdd283c2088983c
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-07 14:49:52 +0900

    カード情報記述に関する編集

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,46 +1,33 @@
-  const pay = () => {
-    const payjp = Payjp('pk_test_7809fa4f0665f83309ba1129');
-    const elements = payjp.elements();
-    
-    
-    const numberElement = elements.create('cardNumber');
-    const expiryElement = elements.create('cardExpiry');
-    const cvcElement = elements.create('cardCvc');
-  
-    
-    numberElement.mount('#number-form');
-    expiryElement.mount('#expiry-form');
-    cvcElement.mount('#cvc-form');
-  
-    const form = document.getElementById('charge-form');
-    form.addEventListener('submit', (e) => {
-      e.preventDefault();
-  
+const pay = () => {
+  const payjp = Payjp('pk_test_7809fa4f0665f83309ba1129')// PAY.JPテスト公開鍵
+  const elements = payjp.elements();
+  const numberElement = elements.create('cardNumber');
+  const expiryElement = elements.create('cardExpiry');
+  const cvcElement = elements.create('cardCvc');
+
+  numberElement.mount('#number-form');
+  expiryElement.mount('#expiry-form');
+  cvcElement.mount('#cvc-form');
+
+  const form = document.getElementById('charge-form')
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    payjp.createToken(numberElement).then(function (response) {
+      if (response.error) {
       
-      payjp.createToken(numberElement).then(function(response) {
-        if (response.error) {
-          
-          alert(response.error.message);
-        } else {
-          
-          const token = response.id;
-  
-          
-          const renderDom = document.getElementById('charge-form');
-          const tokenObj = `<input value=${token} name='token' type='hidden'>`;
-          renderDom.insertAdjacentHTML('beforeend', tokenObj);
-  
-          
-          numberElement.clear();
-          expiryElement.clear();
-          cvcElement.clear();
-  
+      } else {
+        const token = response.id;
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
         
-          document.getElementById('charge-form').submit();
-        }
-      });
+        document.getElementById("charge-form").submit();
+      }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
     });
-  };
-  
-  
-  window.addEventListener('turbo:load', pay);
+  });
+};
+
+window.addEventListener("turbo:load", pay);
--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -5,7 +5,7 @@ const application = Application.start()
 // Configure Stimulus development experience
 import "@hotwired/turbo-rails"
 import "controllers"
-import "card"
+
 
 application.debug = false
 window.Stimulus   = application
