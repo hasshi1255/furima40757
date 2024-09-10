b3ec6617c7ad9fe0a055048f8baefdc0338bdb55
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-10 16:09:27 +0900

    モデル単体テスト編集

---
+++ spec/models/purchase_shipping_spec.rb
@@ -0,0 +1,11 @@
+require 'rails_helper'
+
+RSpec.describe PurchaseShipping, type: :model do
+  describe 'バリデーション' do
+    it 'すべての属性が正しく設定されていると有効であること' do
+      # テスト内容
+    end
+
+    # 他のテストケース
+  end
+end
