35a3a81a518c6e9f16929a22fa8f8fc5bc03468d
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-10 15:40:27 +0900

    items_controllerの編集

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -10,7 +10,7 @@ class ItemsController < ApplicationController
   def create
     @item = Item.new(item_params)
     if @item.save
-      redirect_to root_path, notice: '商品が出品されました'
+      redirect_to root_path
     else
       render :new, status: :unprocessable_entity
     end
