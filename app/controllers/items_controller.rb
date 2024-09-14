6af2e2746cb9acf690e5315410ec307c24a9d120
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-14 13:45:45 +0900

    購入済み商品を編集NGにする

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,6 +2,7 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_item, only: [:show, :edit, :update, :destroy]
   before_action :check_item_owner, only: [:edit, :update, :destroy]
+  before_action :check_item_sold_out, only: [:edit, :update]
 
   def new
     @item = Item.new
@@ -47,6 +48,12 @@ class ItemsController < ApplicationController
 
   def check_item_owner
     redirect_to root_path unless @item.user_id == current_user.id
+  end
+
+  def check_item_sold_out
+    if @item.purchase.present?
+      redirect_to root_path
+    end
   end
 
   def item_params
