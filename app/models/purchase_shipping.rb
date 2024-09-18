4ed9c741cf786cde78066d3c5ce0bc3eccddd2d2
hasshi1255 <hashimoto.r.0901@gmail.com>
2024-09-18 13:07:48 +0900

    with_optionsでまとめ

--- app/models/purchase_shipping.rb
+++ app/models/purchase_shipping.rb
@@ -3,14 +3,16 @@ class PurchaseShipping
   attr_accessor :token, :postal_code, :prefecture_id, :city, :address, :building_name, :phone_number, :user_id, :item_id
 
   # バリデーション
-  validates :user_id, presence: true
-  validates :item_id, presence: true
-  validates :token, presence: true
-  validates :postal_code, presence: true, format: { with: /\A\d{3}-\d{4}\z/ }
+  with_options presence: true do
+    validates :user_id
+    validates :item_id
+    validates :token
+    validates :postal_code, format: { with: /\A\d{3}-\d{4}\z/ }
+    validates :city
+    validates :address
+    validates :phone_number, format: { with: /\A\d{10,11}\z/ }
+  end
   validates :prefecture_id, numericality: { other_than: 1 }
-  validates :city, presence: true
-  validates :address, presence: true
-  validates :phone_number, presence: true, format: { with: /\A\d{10,11}\z/ }
 
   def save
       purchase = Purchase.create!(user_id: user_id, item_id: item_id)
